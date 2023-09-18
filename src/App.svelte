0670b683e29e1e88adb3859f5d5ea56813e24cb3
preinmann <preinmann@mac.com>
2023-09-19 12:14:29 +1300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Router, Route } from 'svelte-routing';
+	import Navbar from './components/Navbar.svelte';
+	import Footer from './components/Footer.svelte';
+	import Home from './routes/Home.svelte';
+	import Logbook from './routes/Logbook.svelte';
+	import Login from './routes/Login.svelte';
+	import Register from './routes/Register.svelte';
+  </script>
+  
+  <Navbar />
+  <Router>
+	<Route path="/" component={Home} />
+	<Route path="/logbook" component={Logbook} />
+	<Route path="/login" component={Login} />
+	<Route path="/register" component={Register} />
+  </Router>  
+  <Footer />
+  
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Link } from 'svelte-routing';
+</script>
+
+<nav>
+  <Link to="/">Home</Link> |
+  <Link to="/logbook">Logbook</Link> |
+  <Link to="/login">Login</Link> |
+  <Link to="/register">Register</Link>
+</nav>
+
+<style>
+  /* Add your styling here */
+</style>
